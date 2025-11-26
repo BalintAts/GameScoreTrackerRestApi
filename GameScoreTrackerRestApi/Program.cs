81b5613c583e42f87faa4fd5e09e2d5d89176a68
atsbalint <atsbalint@DESKTOP-72HFBIG>
2025-11-26 22:16:10 +0100

    handling unhandled exceptions with problems details and logging

--- GameScoreTrackerRestApi/Program.cs
+++ GameScoreTrackerRestApi/Program.cs
@@ -1,6 +1,8 @@
 using DatabaseConnection;
 using GameScoreTrackerRestApi;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
@@ -75,6 +77,8 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
+app.UseExceptionHandler(errorApp => ConfigureExceptionHandler(errorApp, app.Environment));
+
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
@@ -88,3 +92,33 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+
+static void ConfigureExceptionHandler(IApplicationBuilder errorApp, IWebHostEnvironment env)
+{
+    errorApp.Run(async context =>
+    {
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+        if (exception is not null)
+        {
+            logger.LogError(exception, $"Unhandled exception at {context.Request.Path}", context.Request.Path);
+        }
+
+        var detail = env.IsDevelopment() ? exception?.Message : "An unexpected error occurred";
+
+        var problem = new ProblemDetails
+        {
+            Title = "An unexpected error occurred",
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception?.Message,
+            Instance = context.Request.Path
+        };
+
+        problem.Extensions["traceId"] = context.TraceIdentifier;
+
+        context.Response.StatusCode = problem.Status ?? 500;
+        context.Response.ContentType = "application/problem+json";
+        await context.Response.WriteAsJsonAsync(problem);
+    });
+}
